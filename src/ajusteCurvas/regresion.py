b31a7b080b28061ae50a9b92c100ce729ee0236c
Michael Secalos AC <mapazacals@unsa.edu.pe>
2021-12-03 20:15:58 -0500

    Ajuste de curvas cuadratico

---
+++ src/ajusteCurvas/regresion.py
@@ -0,0 +1,40 @@
+from . import gaussElimination
+
+
+def cuadratic(a, b, c, xVal):
+    return a*xVal*xVal + b*xVal + c
+
+
+def regresionCuadratica(xvalues, yvalues):
+    size = len(xvalues)
+    
+    #Lado izquierdo
+    sumX = sum(xvalues)
+    sumX2 = sum([x**2 for x in xvalues])
+    sumX3 = sum([x**3 for x in xvalues])
+    sumX4 = sum([x**4 for x in xvalues])
+    
+    #Lado derecho
+    sumY = sum(yvalues)
+    sumXY = sum([x*y for x,y in zip(xvalues,yvalues)])
+    sumX2Y = sum([x**2*y for x,y in zip(xvalues, yvalues)])
+
+    # La ecuacion tiene la forma: y = a0 + a1*x + a2*x**2
+    # Sistema de ecuaciones
+    # a0*n      +   a1*sumX     +   a2*sumX2 = sumY
+    # a0*sumX   +   a1*sumX2    +   a2*sumX3 = sumXY
+    # a0*sumX2  +   a1*sumX3    +   a2*sumX4 = sumX2Y
+    
+    ecuaciones = [
+        [size, sumX, sumX2, sumY],
+        [sumX, sumX2, sumX3, sumXY],
+        [sumX2, sumX3, sumX4, sumX2Y],
+        ]
+
+    coeficientes = gaussElimination.gaussElimination(ecuaciones)
+
+    chartX = list(range(int(min(xvalues)), int(max(xvalues))))
+    chartY = [cuadratic(coeficientes[2], coeficientes[1], coeficientes[0], x) for x in chartX]
+
+    return chartX, chartY, coeficientes[2], coeficientes[1], coeficientes[0]
+
--- src/ajusteCurvas/views.py
+++ src/ajusteCurvas/views.py
@@ -1,37 +1,33 @@
 from django.http import request
 from django.shortcuts import render
+from . import regresion
 import json
 
-def cuadratic(a, b, c, xVal):
-    return a*xVal*xVal + b*xVal + c
 
-def fit(x,y):
-
+def fit(f_type, xvalues, yvalues):
+    if f_type == 'cuadratic':
+        print(f_type)
     return
 
 # Create your views here.
 def HomeView(request):
     if request.method == 'POST':
-        numberInputs = int((len(request.POST)-1)/2)
-        print(numberInputs)
-        xvalues = []
-        yvalues = []
-        for i in range(numberInputs):
-            xi = request.POST.get('x'+str(i))
-            xvalues.append(0 if xi=='' else int(xi))
-        for i in range(numberInputs):
-            yi = request.POST.get('y'+str(i))
-            yvalues.append(0 if yi=='' else int(yi))
-        a=1
-        b=1
-        c=1
-        #xvalues = [1,2,3,4,5,6,7,8,9]
-        #yvalues = [cuadratic(a,b,c,yi) for yi in xvalues]
+        xvalues = request.POST.get("xvalues")
+        yvalues = request.POST.get("yvalues")
+        if xvalues=="" or yvalues=="":
+            return render(request, 'ajusteCurvas/ajusteCurvas.html', {})
+        xvalues = list(map(float, xvalues.split()))
+        yvalues = list(map(float, yvalues.split()))
+
+        yvalues = [x for y, x in sorted(zip(xvalues, yvalues))]
+        xvalues.sort()
 
         xvalues_json = json.dumps(xvalues)
         yvalues_json = json.dumps(yvalues)
 
-        ctx={'resultado':15, 'xvalues':xvalues_json, 'yvalues':yvalues_json}
+        chartX, chartY, a, b, c = regresion.regresionCuadratica(xvalues, yvalues)
+
+        ctx={'resultado':15, 'xvalues':xvalues_json, 'yvalues':yvalues_json, 'xchart': chartX, 'ychart':chartY, 'a':a, 'b':b, 'c':c}
     else:
         ctx = {}
     return render(request, 'ajusteCurvas/ajusteCurvas.html', ctx)
